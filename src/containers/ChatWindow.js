8364d08f795fc450223895b4a654c21a9d57f34c
Giang Nguyen <tgn1992@hotmail.de>
2018-08-16 23:08:29 +0200

    Final Steps soon

--- src/containers/ChatWindow.js
+++ src/containers/ChatWindow.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import store from '../store/index';
+import { Header } from '../components/Header';
+import { Chats } from '../components/Chats';
+import _ from 'lodash';
+import '../styles/ChatWindow.css';
 
 export const ChatWindow = ({ activeUserId }) => {
+    const state = store.getState();
+    const activeUser = state.contacts[activeUserId];
+    const activeMsgs = state.messages[activeUserId];
+
     return (
         <div className='ChatWindow'>
-            Conversation for user id: {activeUserId}
+            <Header user={activeUser} />
+            <Chats messages={_.values(activeMsgs)} />
         </div>
     );
 };
